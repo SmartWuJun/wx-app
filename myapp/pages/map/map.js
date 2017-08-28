b7e08c5240520d6750c1e5717093775bc56cdcc3
wujun <wujun@xiniunet.com>
2017-08-28 10:33:13 +0800

    update

---
+++ myapp/pages/map/map.js
@@ -0,0 +1,45 @@
+// map.js
+Page({
+  data: {
+    markers: [{
+      iconPath: "//img1.40017.cn/cn/v/2015/zhuanti/28658/icon0.png",
+      id: 0,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      width: 50,
+      height: 50
+    }],
+    polyline: [{
+      points: [{
+        longitude: 113.3245211,
+        latitude: 23.10229
+      }, {
+        longitude: 113.324520,
+        latitude: 23.21229
+      }],
+      color: "#FF0000DD",
+      width: 2,
+      dottedLine: true
+    }],
+    controls: [{
+      id: 1,
+      iconPath: '//pic5.40017.cn/01/001/7d/37/rBLkBVmJGsGAXvuWAARBS0oBPs0969.jpg',
+      position: {
+        left: 0,
+        top: 300 - 50,
+        width: 50,
+        height: 50
+      },
+      clickable: true
+    }]
+  },
+  regionchange(e) {
+    console.log(e.type)
+  },
+  markertap(e) {
+    console.log(e.markerId)
+  },
+  controltap(e) {
+    console.log(e.controlId)
+  }
+})
